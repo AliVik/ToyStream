b2c870f126ea9c21b89c54445b214c36c9df4b5a
AliVik <alvikul1994@gmail.com>
2021-11-27 20:13:08 +0200

    добавляет функцию удаления со списка соц. сетей  visually-hidden

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+
+
+const refs = {
+    mainSocialIcons: document.querySelector('.social-icons'),
+}
+
+let widthMatch = window.matchMedia("(min-width: 1023px)");
+widthMatch.addEventListener('change', removeInvisibility);
+
+
+function removeInvisibility() {
+    if (widthMatch.matches) {
+   return refs.mainSocialIcons.classList.remove('visually-hidden');
+}
+   return refs.mainSocialIcons.classList.add('visually-hidden');
+}
