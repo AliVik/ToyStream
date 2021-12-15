afc4d73fdaa123038bb8c40c50ff0793c0609649
AliVik <alvikul1994@gmail.com>
2021-12-16 00:38:59 +0200

    добавляет функцию появления мобильного меню и закрытия

---
+++ src/js/mobile-menu-backdrop.js
@@ -0,0 +1,20 @@
+export const refs = {
+    burgerMenu: document.querySelector('.burger-menu_button'),
+    mobileMenu:document.querySelector('.mobile-menu'),
+    crossIcon: document.querySelector('.cross-button'),
+    pageHeader: document.querySelector('.flex-header'),
+
+}
+
+refs.burgerMenu.addEventListener('click', onBurgerMenuClick);
+refs.crossIcon.addEventListener('click',onIconCrossClick)
+
+function onBurgerMenuClick() {
+    refs.burgerMenu.classList.add('visually-hidden');
+    refs.mobileMenu.classList.remove('visually-hidden');
+}
+
+function onIconCrossClick() {
+    refs.burgerMenu.classList.remove('visually-hidden');
+    refs.mobileMenu.classList.add('visually-hidden');
+}
