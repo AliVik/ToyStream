c424cf74b1394aa3172bcb2794d82abbf4ea0b6f
AliVik <alvikul1994@gmail.com>
2021-12-29 00:09:59 +0200

    добавляет условие присутствия чекбокса на странице

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -2,7 +2,10 @@ const checkbox = document.querySelector('#checkbox');
 const checkboxIcon = document.querySelector('.policy__svg');
 
 
-checkbox.addEventListener('change', onCheckboxChange);
+if (checkbox) {
+    checkbox.addEventListener('change', onCheckboxChange);
+}
+
 
 function onCheckboxChange() {
     checkboxIcon.classList.toggle('visually-hidden');
